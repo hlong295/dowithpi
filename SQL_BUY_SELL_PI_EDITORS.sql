d0ab4fdb46f7047419f747a1ead510aa8241554a
hlong295 <121739523+hlong295@users.noreply.github.com>
2026-01-13 20:31:17 +0700

    cc

--- SQL_BUY_SELL_PI_EDITORS.sql
+++
@@ -1,3 +0,0 @@
--- Add allowlist for users who can update Pi buy/sell prices (besides admins).
-ALTER TABLE public.app_settings
-ADD COLUMN IF NOT EXISTS pi_exchange_editor_ids text[] DEFAULT '{}'::text[];
